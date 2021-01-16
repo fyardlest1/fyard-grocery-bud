23ebec3eda2820971f730abbfd701857cd765261
fyardlest <fyardlest1@gmail.com>
2021-01-16 18:25:45 -0500

    edit item ok

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ function deleteItem(e) {
     const element = e.currentTarget.parentElement.parentElement
     const id = element.dataset.id
     list.removeChild(element)
-    if (list.children === 0) {
+    if (list.children.length === 0) {
         container.classList.remove('show-container')
     }
     displayAlert('item removed', 'danger')
