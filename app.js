588f50341ec8b2ae6c19ba61136d641198774320
fyardlest <fyardlest1@gmail.com>
2021-01-16 07:43:26 -0500

    GroceryBud - display alert

--- app.js
+++ app.js
@@ -26,8 +26,20 @@ function addItem(e) {
     } else if (value && editFlag) {
         console.log('editing')
     } else {
-        console.log('empty value')
+        displayAlert('please enter value', 'danger')
     }
+}
+
+// display alert
+function displayAlert(text, action) {
+    alert.textContent = text
+    alert.classList.add(`alert-${action}`)
+    
+    // remove alert
+    setTimeout(function() {
+        alert.textContent = ''
+        alert.classList.remove(`alert-${action}`)        
+    }, 2000)
 }
 
 // ****** LOCAL STORAGE **********
