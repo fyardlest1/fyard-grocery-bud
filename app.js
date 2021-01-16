466557d7768c3f1d241f645e9328cd599672bd0a
fyardlest <fyardlest1@gmail.com>
2021-01-16 18:58:48 -0500

    addToLocalStorage function

--- app.js
+++ app.js
@@ -133,7 +133,13 @@ function editItem(e) {
 
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
-    console.log('added to local storage')
+    const grocery = {id, value}
+    let items = localStorage.getItem('list')
+    ? JSON.parse(localStorage.getItem('list')) : []
+    console.log(items)
+    items.push(grocery)
+    localStorage.setItem('list', JSON.stringify(items))
+    // console.log('added to local storage')
 }
 
 function removeFromLocalStorage(id) {
@@ -143,4 +149,9 @@ function removeFromLocalStorage(id) {
 function editLocalStorage(id, value) {
     //
 }
+// localStorage API
+// setItem
+// getItem
+// removeItem
+// save as string
 // ****** SETUP ITEMS **********
